b6fb84a7bf9c4e4869c59d2ad7a1869d1de28368
dliechty <qwertyshoe@gmail.com>
2025-12-24 10:38:56 -0800

    feat: Add OpenAPI client generation and initial generated API client files.

---
+++ src/client/models/Recipe.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type Recipe = {
+    id?: number;
+    title?: string;
+    description?: string;
+    ingredients?: Array<string>;
+    steps?: Array<string>;
+};
+
---
+++ src/client/models/RecipeCreate.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type RecipeCreate = {
+    title: string;
+    description?: string;
+    ingredients?: Array<string>;
+    steps?: Array<string>;
+};
+
