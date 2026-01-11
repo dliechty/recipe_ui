0e6a936d51e7b4dd56c20452d731eb47fdcdb461
dliechty <qwertyshoe@gmail.com>
2026-01-10 16:24:15 -0800

    Add search criteria to client

---
+++ src/client/models/SearchCriterion.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * A single search criterion for meal template SEARCH strategy.
+ */
+export type SearchCriterion = {
+    field: string;
+    operator: string;
+    value: (string | number);
+};
+
